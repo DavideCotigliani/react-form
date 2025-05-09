0db4b92785fafa2691086788443e2d8522174ca0
DavideCotigliani <davide.cotigliani@gmail.com>
2025-05-09 15:49:37 +0200

    [UPD] Set the onChange attribute to define newArticle

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,12 +1,20 @@
+import { useState } from "react"
 
 const Articles = () => {
     const myArticles = ["Shampoo", "Deodorante", "Detergente", "Sapone"]
+
+    const [newArticle, setNewArticle] = useState('')
     return (
-        <ul>
-            {myArticles.map((article, index) => (
-                <li key={index}>{article}</li>
-            ))}
-        </ul>
+        <>
+            <ul>
+                {myArticles.map((article, index) => (
+                    <li key={index}>{article}</li>
+                ))}
+            </ul>
+            <p>Il nuovo articolo è {newArticle}</p>
+            <input type="text" value={newArticle} onChange={e => { setNewArticle(e.target.value) }} />
+        </>
+
     )
 }
 
